566575045766eb8c3ad58d44c04d6739110cfdf5
rortegaez <raulor83@gmail.com>
2023-01-09 07:06:02 +0100

    reto to-do operativo

--- src/containers/Modifier/index.js
+++ src/containers/Modifier/index.js
@@ -3,6 +3,9 @@ import { useForm } from 'react-hook-form'
 import { useLocation } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 
+import styles from './module.modifier.css'
+import ButtonBackMain from '../../components/ButtonBackMain'
+
 const Modifier = () => {
 
 	const { reset, register, handleSubmit, formState: { errors } } = useForm()
@@ -26,15 +29,15 @@ useEffect(() => {
 		console.log('ERROR', error)
 	})
 	let defaultValues = {}
-	defaultValues.title = taskId.title
-	defaultValues.task = taskId.task
-	defaultValues.limit = date
-	reset({ ...defaultValues })
+		defaultValues.title = taskId.title
+		defaultValues.task = taskId.task
+		defaultValues.limit = date
+		reset({ ...defaultValues })
 }, [ idx, reset, taskId.title, taskId.task, date])
 
 const onSubmit = result => {
 	console.log('cambio', result)
-	fetch(`http://localhost:8000/task/63a6cee39cd0b104e3f94bd6`, {
+	fetch(`http://localhost:8000/task/${idx}`, {
 		method: 'PUT',
 		headers: {
 			'Content-Type': 'application/json'
@@ -42,16 +45,11 @@ const onSubmit = result => {
 		body: JSON.stringify(result)
 	})
 	.then(res => res.json())
+	.then(res => window.location())
 	.catch((error) => {
 		console.log(error)
 	})
 }
-
-//console.log('task', taskId)
-
-//console.log('titulo', titleCard)
-
-
 
 	return (
 		<div>
@@ -88,19 +86,23 @@ const onSubmit = result => {
 
 				<h2>Fecha</h2>
 
-				<input type='date' className='containerModifierDate'
-				{...register('limit', {
-					requiered: {
-						value: true,
-						message: 'Debe de tener una fecha límite'
-					}
-				})}
-				/>
-				{ errors.date && <span className='errors'>{ errors.date.message }</span> }
+				<p>
+					<input type='date' className='containerModifierDate'
+					{...register('limit', {
+						requiered: {
+							value: true,
+							message: 'Debe de tener una fecha límite'
+						}
+					})}
+					/>
+					{ errors.date && <span className='errors'>{ errors.date.message }</span> }
+				</p>
 
 				<button className='buttonModifier'>Modifier task</button>
 
 			</form>
+
+			<ButtonBackMain />
 
 		</div>
 	)
